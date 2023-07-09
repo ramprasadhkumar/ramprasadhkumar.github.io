ff0f1c524cc7876b8b4babd28e0fc4741bc0ddc0
Ramprasadh Kumar <ramprasadhkumar@gmail.com>
2023-07-09 18:10:30 -0500

    Added export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const exp = require('constants')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   experimental: {
     appDir: true,
   },
+  
 }
 
 module.exports = nextConfig
